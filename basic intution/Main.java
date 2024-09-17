c04057ea4da472f441a23cf6f5277a2feb771213
xxARYANx <aryanlappy2022@gmail.com>
2024-09-18 00:41:17 +0530

    add dsa solutions

---
+++ basic intution/Main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=0;i<t;i++){
+            int x=s.nextInt();
+            for(int y=1;y<x;y++){
+                    for(int rem=1;rem<=y;rem++){
+                        if(x%rem==0 && y%rem==0 && rem+y==x){
+                            System.out.println(y);
+                           
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
---
+++ basic intution/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+int red=s.nextInt();
+        // int blue=s.nextInt();
+        // int green=s.nextInt();
